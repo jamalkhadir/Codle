8492817e8b7c5e003f567587f267b72e8cd69a46
Jamal <jamalkhadir@gmail.com>
2022-10-24 16:32:18 -0400

    Word scope

--- src/Word.js
+++ src/Word.js
@@ -5,7 +5,7 @@ export const wordsetGenerator = async() => {
     let possible;
     let currentWord;
     await fetch(choices).then((response) => response.text()).then((result) => {
-        const possibilitiesArr = result.split("\n")
+        const possibilitiesArr = result.split(/\r?\n/)
         currentWord = possibilitiesArr[Math.floor(Math.random() * possibilitiesArr.length)]
         possible = new Set(possibilitiesArr)
     })
