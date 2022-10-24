51f0729d40ab28386c4d4d18251013380391c506
Jamal <jamalkhadir@gmail.com>
2022-10-24 13:38:16 -0400

    Possibilities update 1

--- src/Word.js
+++ src/Word.js
@@ -5,7 +5,7 @@ export const wordsetGenerator = async() => {
     let possible;
     let currentWord;
     await fetch(choices).then((response) => response.text()).then((result) => {
-        const possibilitiesArr = result.split("\r\n")
+        const possibilitiesArr = result.split("\n")
         currentWord = possibilitiesArr[Math.floor(Math.random() * possibilitiesArr.length)]
         possible = new Set(possibilitiesArr)
     })
